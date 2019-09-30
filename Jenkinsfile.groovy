374698de37d965c724b53cbe1728212f3e84187b
nguyenhainam <hainam.nguyen@nfq.asia>
2019-09-30 21:29:26 +0700

    revert changes

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,9 +2,9 @@ node {
   checkout scm
   switch(env.BRANCH_NAME) {
     case "develop":
-      build job: 'staging', parameters: [[$class: 'StringParameterValue', name: 'BRANCH', value: "${env.BRANCH_NAME}"]]
+      build job: 'staging', parameters: [[$class: 'StringParameterValue', name: 'BRANCH_NAME', value: "${env.BRANCH_NAME}"]]
     break
-
+    
     default:
     break
   }
